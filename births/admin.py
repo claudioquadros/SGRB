42adf5f1d626eb76a23d140d7412a391ef448b3f
claudioquadros <claudio.quadros@gmail.com>
2025-08-17 14:43:44 -0300

    second commit

--- births/admin.py
+++ births/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Birth
 
-# Register your models here.
+
+@admin.register(Birth)
+class BirthAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'animal',
+        'insemination',
+        'expected_birth',
+        'birth',
+        'created_at',
+        'updated_at',
+    )
+    list_filter = (
+        'animal__farm__company',
+        'animal__breed',
+        'animal__category',
+        'expected_birth',
+        'birth',
+    )
+    search_fields = (
+        'animal__name',
+        'insemination__animal__name',
+    )
+    date_hierarchy = 'birth'
+
+    readonly_fields = (
+        'created_at',
+        'updated_at',
+    )
--- inseminations/admin.py
+++ inseminations/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Insemination
 
-# Register your models here.
+
+@admin.register(Insemination)
+class InseminationAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'animal',
+        'insemination_date',
+        'expected_birth',
+        'result',
+        'created_at',
+        'updated_at',
+    )
+    list_filter = (
+        'result',
+        'animal__farm__company',
+        'animal__breed',
+        'animal__category',
+        'insemination_date',
+        'expected_birth',
+    )
+    search_fields = (
+        'animal__name',
+        'animal__farm__name',
+    )
+    date_hierarchy = 'insemination_date'
+
+    readonly_fields = (
+        'created_at',
+        'updated_at',
+    )
