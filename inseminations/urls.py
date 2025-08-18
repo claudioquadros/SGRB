9c6413496d172cbbf7273ea98e085c567dda96e8
claudioquadros <claudio.quadros@gmail.com>
2025-08-18 17:01:32 -0300

    6 commit

--- inseminations/urls.py
+++ inseminations/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 
-app_name = 'inseminations'
-
 urlpatterns = [
     path('inseminations/list/', views.InseminationListView.as_view(), name='insemination_list'),  # noqa
     path('inseminations/create/', views.InseminationCreateView.as_view(), name='insemination_create'),  # noqa
